79b7a1d410b1ad382e2f304fba04677f4f616652
awrigh206 <awrigh206@caledonian.ac.uk>
2021-08-19 16:48:59 +0100

    Making gallery resize with screen size

--- lib/Widgets/Gallery.dart
+++ lib/Widgets/Gallery.dart
@@ -16,43 +16,62 @@ class Gallery extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 50),
       child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Card(
-          borderOnForeground: true,
-          elevation: 20.0,
-          shadowColor: Colors.blueGrey,
-          child: GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 450,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15),
-              itemCount: project.images.length,
-              itemBuilder: (context, index) {
-                FadeInImage currentImage = FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: project.images[index].route,
-                );
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SingleImageRoute(
-                                    displayImage: currentImage,
-                                    index: index,
-                                  )));
-                    },
-                    child: Hero(
-                        tag: 'image' + index.toString(), child: currentImage),
-                  ),
-                );
-              }),
-        ),
-      ),
+          padding: const EdgeInsets.all(10.0),
+          child: Builder(builder: (context) {
+            var screenSize = MediaQuery.of(context).size;
+            if (screenSize.width > 900.0) {
+              return Card(
+                borderOnForeground: true,
+                elevation: 20.0,
+                shadowColor: Colors.blueGrey,
+                child: ImageGrid(project: project),
+              );
+            } else {
+              return ImageGrid(project: project);
+            }
+          })),
     );
+  }
+}
+
+class ImageGrid extends StatelessWidget {
+  const ImageGrid({
+    Key? key,
+    required this.project,
+  }) : super(key: key);
+
+  final Project project;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 450,
+            childAspectRatio: 1,
+            crossAxisSpacing: 15,
+            mainAxisSpacing: 15),
+        itemCount: project.images.length,
+        itemBuilder: (context, index) {
+          FadeInImage currentImage = FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: project.images[index].route,
+          );
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SingleImageRoute(
+                              displayImage: currentImage,
+                              index: index,
+                            )));
+              },
+              child: Hero(tag: 'image' + index.toString(), child: currentImage),
+            ),
+          );
+        });
   }
 }
