555b7e4ee8bf4755e081e971897680604dddc668
awrigh206 <awrigh206@caledonian.ac.uk>
2021-08-23 16:54:50 +0100

    Wave animation on hover of top bar

--- lib/Helpers/WaveClipper.dart
+++ lib/Helpers/WaveClipper.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 class WaveClipper extends CustomClipper<Path> {
+  final animationValue;
+
+  const WaveClipper(this.animationValue);
   @override
   Path getClip(Size size) {
     var path = new Path();
     path.lineTo(0, size.height);
 
-    var firstStart = Offset(size.width / 5, size.height);
+    var firstStart = Offset(size.width / 5 - animationValue, size.height);
     //fist point of quadratic bezier curve
-    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+    var firstEnd =
+        Offset(size.width / 2.25 - animationValue, size.height - 50.0);
     //second point of quadratic bezier curve
     path.quadraticBezierTo(
         firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
 
-    var secondStart =
-        Offset(size.width - (size.width / 3.24), size.height - 105);
+    var secondStart = Offset(
+        size.width - (size.width / 3.24) - animationValue, size.height - 105);
     //third point of quadratic bezier curve
     var secondEnd = Offset(size.width, size.height - 10);
     //fourth point of quadratic bezier curve
@@ -28,6 +32,6 @@ class WaveClipper extends CustomClipper<Path> {
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
+    return true;
   }
 }
--- lib/Widgets/WaveWidget.dart
+++ lib/Widgets/WaveWidget.dart
@@ -1,40 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:showcase/Helpers/WaveClipper.dart';
 
-class WaveWidget extends StatelessWidget implements PreferredSizeWidget {
-  const WaveWidget({Key? key, required this.focusWidget}) : super(key: key);
+class WaveWidget extends StatefulWidget implements PreferredSizeWidget {
+  WaveWidget({Key? key, required this.focusWidget}) : super(key: key);
   final Widget focusWidget;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: Stack(
-      children: <Widget>[
-        Opacity(
-          opacity: 0.5,
-          child: ClipPath(
-            clipper: WaveClipper(),
-            child: Container(
-              color: Theme.of(context).accentColor,
-              height: 200,
-            ),
-          ),
-        ),
-        ClipPath(
-            clipper: WaveClipper(),
-            child: Container(
-              padding: EdgeInsets.only(bottom: 50),
-              color: Theme.of(context).primaryColor,
-              height: 180,
-              alignment: Alignment.center,
-              child: focusWidget,
-            )),
-      ],
-    ));
-  }
+  _WaveWidgetState createState() => _WaveWidgetState();
 
   @override
   Size get preferredSize {
-    return Size.fromHeight(200.0);
+    return Size.fromHeight(150.0);
+  }
+}
+
+class _WaveWidgetState extends State<WaveWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation waveAnimation;
+  late double value;
+
+  @override
+  void initState() {
+    super.initState();
+    value = 100.0;
+    controller =
+        AnimationController(duration: const Duration(seconds: 10), vsync: this);
+    waveAnimation = Tween(begin: 90.0, end: 500.0).animate(controller);
+
+    waveAnimation.addListener(() {
+      setState(() {
+        value = waveAnimation.value;
+      });
+      if (waveAnimation.isCompleted) {
+        controller.reverse();
+      } else if (waveAnimation.isDismissed) {
+        controller.forward();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onDoubleTap: () {},
+        onHover: (isHovering) {
+          if (isHovering) {
+            controller.forward();
+          } else {
+            controller.stop();
+          }
+        },
+        child: Container(
+            child: Stack(
+          children: <Widget>[
+            Opacity(
+              opacity: 0.5,
+              child: ClipPath(
+                clipper: WaveClipper(value),
+                child: Container(
+                  color: Theme.of(context).accentColor,
+                  height: 150,
+                ),
+              ),
+            ),
+            ClipPath(
+                clipper: WaveClipper(waveAnimation.value),
+                child: Container(
+                  padding: EdgeInsets.only(bottom: 50),
+                  color: Theme.of(context).primaryColor,
+                  height: 125,
+                  alignment: Alignment.center,
+                  child: widget.focusWidget,
+                )),
+          ],
+        )),
+      ),
+    );
   }
 }
